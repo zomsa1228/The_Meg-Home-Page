a923c8aca5cef29193fc74900f0f82bf0fe5ad83
zomsa1228 <Adventure1611@gmail.com>
2021-01-01 20:37:25 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# this is the [RADAR'S CO]'s homepages
+
+サイトのバグについてはTwitterアカウント@zomasaまでご連絡下さい
---
+++ text/rule.php
@@ -0,0 +1,17 @@
+<?php
+// ファイルを読み込み専用でオープンする
+$fp = fopen('rule.txt', 'r');
+// 終端に達するまでループ
+while (!feof($fp)) {
+// ファイルから一行読み込む
+$line = fgets($fp);
+// 読み込んだ行を出力する
+print $line;
+// <br>の出力
+print "<br>\n";
+// <hr>の出力
+print "<hr>\n";
+}
+// ファイルをクローズする
+close($fp);
+?>
